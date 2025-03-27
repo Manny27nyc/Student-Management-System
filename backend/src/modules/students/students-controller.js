5a329f691868820fb68e6ef00fb1893250cbfee1
Oleg Abrosimov <olegabrosimovnsk@gmail.com>
2025-03-27 15:40:21 +0700

    problem 2 fix

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -3,27 +3,40 @@ const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updat
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
     //write your code
-
+    const payload = req.query;
+    const students = await getAllStudents(payload);
+    res.json({ students });
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
     //write your code
-
+    const payload = req.body;
+    const message = await addNewStudent(payload);
+    res.json(message);
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
     //write your code
-
+    const { id: userId } = req.params;
+    const payload = req.body;
+    const message = await updateStudent({ ...payload, userId });
+    res.json(message);
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
     //write your code
-
+    const { id: userId } = req.params;
+    const message = await getStudentDetail(userId);
+    res.json(message);
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
     //write your code
-
+    const payload = req.body;
+    const { id: userId } = req.params;
+    const { id: reviewerId } = req.user;
+    const message = await setStudentStatus({ ...payload, userId, reviewerId });
+    res.json(message);
 });
 
 module.exports = {
